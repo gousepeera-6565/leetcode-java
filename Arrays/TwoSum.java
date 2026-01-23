a977cb98843e74f64d3eb0c3fe88eec8d368dd7b
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-01-23 22:58:05 +0530

    Two Sum (Easy Method)

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -18,3 +18,25 @@ class TwoSum {
         return new int[]{};
     }
 }
+
+
+// LeetCode: Two Sum
+// Difficulty: Easy
+// Approach: Brute Force (Two Loops)
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
