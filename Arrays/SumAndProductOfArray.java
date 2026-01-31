e6fa2cead2b17994939b24fcb4f84befd18fb217
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-01-31 21:00:36 +0530

    Add sum method to calculate array elements' sum
    Added a method to calculate the sum of array elements and integrated it into the main method.

--- Arrays/SumAndProductOfArray.java
+++ Arrays/SumAndProductOfArray.java
@@ -13,3 +13,32 @@ public class SumAndProductOfArray {
         }
         return p;
     }
+   // Method to calculate sum of array elements
+    static int sum(int[] ar) {
+        int s = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+            s += ar[i];
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scan.nextInt();
+
+        int[] ar = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = scan.nextInt();
+        }
+
+        System.out.println("Sum of array elements: " + sum(ar));
+        System.out.println("Product of array elements: " + product(ar));
+
+        scan.close();
+    }
+}
