b7e8586eb9e73d3694c3a6f0af15d58e51d97998
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-02-02 22:28:24 +0530

    Add sum method to calculate total array sum

--- Arrays/ArraySumExcludingMinMax.java
+++ Arrays/ArraySumExcludingMinMax.java
@@ -22,7 +22,14 @@ public class ArraySumExcludingMinMax {
         return min;
     }
 
-    
+    static int sum(int[] ar) {
+        int sum = 0;
+        for (int i = 0; i < ar.length; i++) {
+            sum += ar[i];
+        }
+        return sum;
+    }
+
 
 
   public static void main(String[] args) {
@@ -34,7 +41,7 @@ public class ArraySumExcludingMinMax {
         for (int i = 0; i < ar.length; i++) {
             ar[i] = scan.nextInt();
         }
-
+         int totalSum = sum(ar);
           int maxValue = max(ar);
            int minValue = min(ar);
 
@@ -42,6 +49,11 @@ public class ArraySumExcludingMinMax {
       
         // Sum excluding maximum element
         System.out.println("Sum excluding maximum: " + (totalSum - maxValue));
+
+      // Sum excluding minimum element
+        System.out.println("Sum excluding minimum: " + (totalSum - minValue));
+
+        scan.close();
 
   }
 }
