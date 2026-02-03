8f326801d09a99cee07c406f94eb4e3290263fdb
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-02-03 21:29:08 +0530

    Implement method to print all pairs from an array
    Adds a method to print all possible pairs from an array.

---
+++ Arrays/ArrayPairs.java
@@ -0,0 +1,45 @@
+ 
+
+import java.util.Scanner;
+
+public class ArrayPairs {
+
+    // Method to print all possible pairs
+    static void printAllPairs(int[] ar) {
+        System.out.println("All possible pairs:");
+        for (int i = 0; i < ar.length - 1; i++) {
+            for (int j = i + 1; j < ar.length; j++) {
+                System.out.println(ar[i] + " " + ar[j]);
+            }
+        }
+    }
+
+    
+
+        if (!found) {
+            System.out.println("No pairs found");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = scan.nextInt();
+
+        int[] ar = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = scan.nextInt();
+        }
+
+        System.out.print("Enter target sum (k): ");
+        int k = scan.nextInt();
+
+        printAllPairs(ar);
+        
+
+        scan.close();
+    }
+}
