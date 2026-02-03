ca95b91f5b9c9bceddc59159f846d00133ae0a40
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-02-03 21:30:14 +0530

    Add method to print pairs with a specific sum

--- Arrays/ArrayPairs.java
+++ Arrays/ArrayPairs.java
@@ -13,7 +13,19 @@ public class ArrayPairs {
             }
         }
     }
+    // Method to print pairs whose sum equals k
+    static void printPairsWithSum(int[] ar, int k) {
+        System.out.println("Pairs with sum " + k + ":");
+        boolean found = false;
 
+        for (int i = 0; i < ar.length - 1; i++) {
+            for (int j = i + 1; j < ar.length; j++) {
+                if (ar[i] + ar[j] == k) {
+                    System.out.println(ar[i] + " " + ar[j]);
+                    found = true;
+                }
+            }
+        }
     
 
         if (!found) {
@@ -38,6 +50,7 @@ public class ArrayPairs {
         int k = scan.nextInt();
 
         printAllPairs(ar);
+        printPairsWithSum(ar, k);
         
 
         scan.close();
