ada677465092d2354f9558524f7a603023e54aca
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-02-12 23:10:42 +0530

    Add palindrome check method using two-pointer approach

--- Strings/StringReversePalindrome.java
+++ Strings/StringReversePalindrome.java
@@ -35,6 +35,22 @@ public class Day19_StringReversePalindrome {
         return result;
     }
 
+
+ // Method to check palindrome using two-pointer approach
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
     
 
     public static void main(String[] args) {
@@ -46,7 +62,13 @@ public class Day19_StringReversePalindrome {
         String reversed = reverse(input);
         System.out.println("Reversed String: " + reversed);
 
-         
+           // Check palindrome
+        if (isPalindrome(input)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
 
          
     }
