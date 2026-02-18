4ad620d6a3641dd6479cd111fc6a022d6ec73cd9
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-02-18 19:16:50 +0530

    Add custom string split method in StringSplitCustom
    Implemented a custom string splitting method that separates words without using the built-in split() function. The main method allows user input and displays the split words.

---
+++ Strings/StringSplitCustom.java
@@ -0,0 +1,48 @@
+ package gousepeera;
+import java.util.*;
+
+public class StringSplitCustom {
+
+    // Method to split string without using built-in split()
+    public static String[] split(String s) {
+        int count = countWords(s);
+        String[] arr = new String[count];
+
+        String temp = "";
+        int index = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                temp = temp + s.charAt(i);
+            } else {
+                if (!temp.equals("")) {
+                    arr[index] = temp;
+                    index++;
+                    temp = "";
+                }
+            }
+        }
+
+        // Add last word
+        if (!temp.equals("")) {
+            arr[index] = temp;
+        }
+
+        return arr;
+    }
+
+  
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.nextLine();
+
+        String[] result = split(s);
+
+        for (String word : result) {
+            System.out.println(word);
+        }
+
+        scan.close();
+    }
+}
