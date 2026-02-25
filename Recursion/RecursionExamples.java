21888b162aa7d0100c632bda100b0f493cc337aa
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-02-25 22:48:46 +0530

    Add RecursionExamples class with multiple examples
    Added a class with various recursion examples including methods for printing numbers in descending order.

---
+++ Recursion/RecursionExamples.java
@@ -0,0 +1,41 @@
+package Dsa_Java;
+
+import java.util.Scanner;
+
+/**
+  
+ * Recursion Examples:
+ * 1. Print numbers
+ * 2. Factorial
+ * 3. Fibonacci
+ * 4. Natural numbers
+ * 5. Count digits
+ * 6. Sum of digits
+ * 7. Reverse string
+ * 8. Palindrome check
+ * 9. Sum of array
+ * 10. Josephus problem
+ * 11. Subset sum count
+ */
+public class RecursionExamples {
+
+    // 1. Print numbers from n to 1
+    public static void printDesc(int n) {
+        if (n <= 0) return;
+        System.out.println(n);
+        printDesc(n - 1);
+    }
+
+   
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+       printDesc(9);
+
+        
+    }
+}
