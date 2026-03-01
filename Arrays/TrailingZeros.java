0297e5914905dde2028ba65ea5ee231db9459967
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-03-01 20:56:47 +0530

    Update TrailingZeros.java

--- Arrays/TrailingZeros.java
+++ Arrays/TrailingZeros.java
@@ -27,8 +27,7 @@ public class TrailingZeros {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
+         System.out.print("Enter a number: ");
         int num = scan.nextInt();
 
         System.out.println("Trailing zeros in factorial: " + trailingZeros(num));
