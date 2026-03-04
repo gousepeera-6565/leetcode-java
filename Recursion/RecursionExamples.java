5d5600e2974e63a473056cd0de3df567d35d5e4e
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-03-04 23:20:37 +0530

    Add Josephus Problem implementation

--- Recursion/RecursionExamples.java
+++ Recursion/RecursionExamples.java
@@ -74,6 +74,12 @@ public class RecursionExamples {
         return arr[i] + sumArray(arr, i + 1);
     }
 
+   // 10. Josephus Problem
+    public static int josephus(int n, int k) {
+        if (n == 1) return 0;
+        return (josephus(n - 1, k) + k) % n;
+    }
+
   
     public static void main(String[] args) {
 
