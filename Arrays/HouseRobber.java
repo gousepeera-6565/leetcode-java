9b3f5cacbaa37c75b1660e100b99ba567b7bbb2e
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-03-26 21:45:31 +0530

    Update HouseRobber.java

--- Arrays/HouseRobber.java
+++ Arrays/HouseRobber.java
@@ -27,6 +27,35 @@ public class HouseRobber {
         return Math.max(rob, noRob);
     }
 
+
+
+/**
+ 
+ * Problem: House Robber (Alternative Approach)
+ * 
+ * Using Dynamic Programming Array
+ */
+ public static int rob(int[] nums) {
+
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        // dp[i] = max money that can be robbed till house i
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            // Either skip current house or rob it
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[nums.length - 1];
+    }
+
+
+ 
     public static void main(String[] args) {
 
         int[] nums = {2, 7, 9, 3, 1};
