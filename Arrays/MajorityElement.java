08c2376c13872c038488c32de10030672b9d665b
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-04-10 19:38:33 +0530

    Add majorityElement method to find majority element
    Implement Boyer-Moore Voting Algorithm to find majority element.

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -1,1 +1,22 @@
+ class Solution {
+    public int majorityElement(int[] a) {
+        
+        int m= a[0];
+        int c=1;
+        for(int i = 0; i < a.length; i++){
+            if(m == a[i]){
 
+                c++;
+            } 
+            else{
+                c--;
+            }
+            if (c==0){
+                m=a[i];
+                c=1;
+            }
+
+        }
+        return m;
+    }
+}
