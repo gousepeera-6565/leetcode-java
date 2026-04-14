ac35e9f925c237ee6a1d1437b82f7fedfcd019f4
DUDEKUL GOUSE PEERA <gousepeera9381@gmail.com>
2026-04-14 21:47:33 +0530

    Update attendance record checking logic
    Refactor attendance check logic to improve clarity and correctness.

--- Strings/Student Attendance Record I.java
+++ Strings/Student Attendance Record I.java
@@ -1,3 +1,35 @@
+
+class Solution {
+    public boolean checkRecord(String s) {
+        int a=0;
+        int y=1;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='A'){
+                a++;
+            }
+        }
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='L' && s.charAt(i+1)=='L'){
+                y++;
+                System.out.println(i+""+(i+1)+""+y);
+            }
+            else if(y>=3){
+                break;
+            }
+            else{
+                y=1;
+            }
+        }
+        if(a>=2 || y>=3){
+            return false;
+        }
+        return true;
+    }
+}
+
+
+
+
 Example 1:
 
 Input: s = "PPALLP"
